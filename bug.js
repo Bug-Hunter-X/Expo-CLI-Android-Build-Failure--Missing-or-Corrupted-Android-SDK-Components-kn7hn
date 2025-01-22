90d73deeb1e00c73705b2bd50540faa982eb6cb6
BugHunter-X <askzenai@gmail.com>
2025-01-23 00:46:06 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,1 @@
+This bug occurs when using the Expo CLI to build an Android APK.  The build process fails with an error related to missing or corrupted Android SDK components. Specifically, the error message mentions issues with the build-tools or platform-tools. This problem is not always consistently reproducible across different machines or Expo CLI versions.
